6f7521e861da57a126a37be6cef84cc900eeab24
Guto Xavier <gutoffline@gmail.com>
2025-07-28 18:41:25 -0300

    componente card

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
+import Card from './Card';
+
 
 function App() {
 
   return (
     <>
-      <div className='card'>
-        <h2>Rick Sanchez</h2>
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
-      </div>
+      <Card />
+      <Card />
+      <Card />
     </>
   )
 }
