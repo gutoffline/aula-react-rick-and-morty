9515beff16354d4b82419f7fb368b2d3565065ed
Guto Xavier <gutoffline@gmail.com>
2025-07-28 18:53:43 -0300

    propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ function App() {
 
   return (
     <>
-      <Card />
-      <Card />
-      <Card />
+      <Card nome='Rick Sanchez' foto='https://rickandmortyapi.com/api/character/avatar/1.jpeg' />
+      <Card nome='Morty Smith' foto='https://rickandmortyapi.com/api/character/avatar/2.jpeg' />
+      <Card nome='Summer Smith' foto='https://rickandmortyapi.com/api/character/avatar/3.jpeg' />
     </>
   )
 }
