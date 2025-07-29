9b38a4320db5cf952ee331dbff921cb65877e48f
Guto Xavier <gutoffline@gmail.com>
2025-07-29 09:59:35 -0300

    criação de personagens

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,16 @@ import Card from './Card';
 
 
 function App() {
-  const item = {  nome: 'Rick Sanchez', foto: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg' }
+  const item1 = {  nome: 'Rick Sanchez', foto: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg' }
+  const item2 = {  nome: 'Morty Smith', foto: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg' }
+  const item3 = {  nome: 'Summer Smith', foto: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg' }
+
+   
   return (
     <>
-      <Card info={item} />
+      <Card info={item1} />
+      <Card info={item2} />
+      <Card info={item3} />
     </>
   )
 }
