935eb2b8ec59c3a15ee818b40e958fa16a1351a7
Guto Xavier <gutoffline@gmail.com>
2025-07-29 09:53:50 -0300

    criação de objeto

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,10 @@ import Card from './Card';
 
 
 function App() {
-
+  const item = {  nome: 'Rick Sanchez', foto: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg' }
   return (
     <>
-      <Card nome='Rick Sanchez' foto='https://rickandmortyapi.com/api/character/avatar/1.jpeg' />
-      <Card nome='Morty Smith' foto='https://rickandmortyapi.com/api/character/avatar/2.jpeg' />
-      <Card nome='Summer Smith' foto='https://rickandmortyapi.com/api/character/avatar/3.jpeg' />
+      <Card info={item} />
     </>
   )
 }
