b0dfb83c5c407442c81d45e537f0d63626f8e31d
Guto Xavier <gutoffline@gmail.com>
2025-07-29 10:31:59 -0300

    ajuste na visualização dos persagens

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,32 @@ import Card from './Card';
 
 
 function App() {
-  const item1 = {  
-    nome: 'Rick Sanchez', 
-    foto: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg' 
-  }
-  
-  const item2 = {  
-    nome: 'Morty Smith', 
-    foto: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg' 
+  const item1 = {
+    nome: 'Rick Sanchez',
+    foto: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
   }
 
-  const item3 = {  
-    nome: 'Summer Smith', 
-    foto: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg' 
+  const item2 = {
+    nome: 'Morty Smith',
+    foto: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
   }
-   
+
+  const item3 = {
+    nome: 'Summer Smith',
+    foto: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+  }
+
   const personagens = [item1, item2, item3]
 
   return (
     <>
-      {
-        personagens.map(function (umPersonagem){
-          return <Card info={umPersonagem} />
-        })
-      }
+      <div className='lista'>
+        {
+          personagens.map(function (umPersonagem) {
+            return <Card info={umPersonagem} />
+          })
+        }
+      </div>
     </>
   )
 }
