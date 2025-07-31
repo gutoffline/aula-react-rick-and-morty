a88e6c90310855784119c935a3610ac099a7c25d
Guto Xavier <gutoffline@gmail.com>
2025-07-31 16:59:30 -0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    base: '/aula-react-rick-and-morty/', // Substitua pelo nome do seu repositório
+
 })
